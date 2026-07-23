66cd4848ef9e4a7090faff6a50863550a8fda5fc
AldoBC007 <91302525+AldoBC007@users.noreply.github.com>
2026-07-23 17:02:41 -0600

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { TaskStore } from './features/tasks/task-store';
 import { RouterOutlet } from '@angular/router';
-import { Task } from './task';
+//import { Task } from './features/tasks/task';
 //import { Task } from 'task-plugin-package';
 
 @Component({
@@ -12,31 +13,5 @@ import { Task } from './task';
 export class App {
   protected readonly title = 'Gestor de Tareas';
 
-  tareas = signal<Task[]>([
-    { id: 1, titulo: 'Aprender Angular', completada: false },
-    { id: 2, titulo: 'Construir Proyecto', completada: false },
-    { id: 3, titulo: 'Dominar signals', completada: true },
-  ]);
-
-  agregar(titulo: string): void {
-    const limpio = titulo.trim();
-    if (!limpio) {
-      return;
-    }
-    this.tareas.update((lista) => [
-      ...lista,
-      { id: Date.now(), titulo: limpio, completada: false },
-    ]);
-  }
-
-  toggle(id: number): void {
-    this.tareas.update((lista) =>
-      lista.map((t) => (t.id === id ? { ...t, completada: !t.completada } : t))
-    );
-  }
-
-  //Tarea 1
-  eliminar(id: number): void {
-    this.tareas.update((tareas) => tareas.filter((tarea) => tarea.id !== id));
-  }
+  store = inject(TaskStore);
 }
