3dcd3f757be0f789132d8cb6fe90968eee66bd2f
AldoBC007 <91302525+AldoBC007@users.noreply.github.com>
2026-07-29 17:10:08 -0600

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -2,12 +2,14 @@ import { Component, inject } from '@angular/core';
 import { TaskStore } from './features/tasks/task-store';
 import { RouterOutlet } from '@angular/router';
 import { TaskItem } from './shared/ui/task-item/task-item';
+//Tarea 3
+import { TaskForm } from './shared/ui/task-form/task-form';
 //import { Task } from './features/tasks/task';
 //import { Task } from 'task-plugin-package';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskItem],
+  imports: [RouterOutlet, TaskItem, TaskForm],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
