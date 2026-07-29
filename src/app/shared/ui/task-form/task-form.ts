d072bb10c64dfe2ad141733e358e0377345cd9e3
AldoBC007 <91302525+AldoBC007@users.noreply.github.com>
2026-07-29 17:08:22 -0600

    Update task-form.ts

--- src/app/shared/ui/task-form/task-form.ts
+++ src/app/shared/ui/task-form/task-form.ts
@@ -1,1 +1,14 @@
+import { Component, input, Input, output } from '@angular/core';
+import { Task } from '../../../features/tasks/task';
 
+@Component({
+  selector: 'app-task-form',
+  imports: [],
+  templateUrl: './task-form.html',
+  styleUrl: './task-form.css',
+})
+export class TaskForm {
+  //task = input.required<Task>();
+  //Tarea 3
+  add = output<string>();
+}
