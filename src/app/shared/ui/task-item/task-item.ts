93a53112b38f9d451bed749485662aba5580e19f
AldoBC007 <91302525+AldoBC007@users.noreply.github.com>
2026-08-07 17:20:59 -0600

    Update task-item.ts

--- src/app/shared/ui/task-item/task-item.ts
+++ src/app/shared/ui/task-item/task-item.ts
@@ -1,9 +1,10 @@
 import { Component, input, Input, output } from '@angular/core';
 import { Task } from '../../../features/tasks/task';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-task-item',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './task-item.html',
   styleUrl: './task-item.css',
 })
