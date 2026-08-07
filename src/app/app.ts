37fdff350699915a9e11604fd657906195d7a72a
AldoBC007 <91302525+AldoBC007@users.noreply.github.com>
2026-08-07 10:53:43 -0600

    Update app.ts

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,20 +1,19 @@
 import { Component, inject } from '@angular/core';
-import { TaskStore } from './features/tasks/task-store';
+//import { TaskStore } from './features/tasks/task-store';
 import { RouterOutlet } from '@angular/router';
-import { TaskItem } from './shared/ui/task-item/task-item';
+//import { TaskItem } from './shared/ui/task-item/task-item';
 //Tarea 3
-import { TaskForm } from './shared/ui/task-form/task-form';
+//import { TaskForm } from './shared/ui/task-form/task-form';
 //import { Task } from './features/tasks/task';
 //import { Task } from 'task-plugin-package';
 
+//Tarea 3
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, TaskItem, TaskForm],
+  imports: [RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.css',
 })
 export class App {
   protected readonly title = 'Gestor de Tareas';
-
-  store = inject(TaskStore);
 }
