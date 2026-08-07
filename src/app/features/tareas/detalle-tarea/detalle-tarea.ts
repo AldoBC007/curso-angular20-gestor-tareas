92783b621dcc5a1a0706ec3461c8e199035e105e
AldoBC007 <91302525+AldoBC007@users.noreply.github.com>
2026-08-07 16:45:13 -0600

    Update detalle-tarea.ts

--- src/app/features/tareas/detalle-tarea/detalle-tarea.ts
+++ src/app/features/tareas/detalle-tarea/detalle-tarea.ts
@@ -1,1 +1,16 @@
+import { Component, computed, inject, input } from '@angular/core';
+import { TaskStore } from '../../tasks/task-store';
 
+@Component({
+  selector: 'app-detalle-tarea',
+  imports: [],
+  templateUrl: './detalle-tarea.html',
+  styleUrl: './detalle-tarea.css',
+})
+export class DetalleTarea {
+  private store = inject(TaskStore);
+
+  id = input.required<string>();
+
+  tarea = computed(() => this.store.tareas().find((t) => t.id === Number(this.id())));
+}
