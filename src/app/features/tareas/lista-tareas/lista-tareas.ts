7335f4a9139fafc310cbba274c41659b6fc55d3c
AldoBC007 <91302525+AldoBC007@users.noreply.github.com>
2026-08-07 10:51:55 -0600

    Update lista-tareas.ts

--- src/app/features/tareas/lista-tareas/lista-tareas.ts
+++ src/app/features/tareas/lista-tareas/lista-tareas.ts
@@ -1,1 +1,15 @@
+import { Component, inject } from '@angular/core';
+import { TaskItem } from '../../../shared/ui/task-item/task-item';
+import { TaskForm } from '../../../shared/ui/task-form/task-form';
+import { TaskStore } from '../../tasks/task-store';
+//import { TaskStore } from '../task-store';
 
+@Component({
+  selector: 'app-lista-tareas',
+  imports: [TaskItem, TaskForm],
+  templateUrl: './lista-tareas.html',
+  styleUrl: './lista-tareas.css',
+})
+export class ListaTareas {
+  store = inject(TaskStore);
+}
